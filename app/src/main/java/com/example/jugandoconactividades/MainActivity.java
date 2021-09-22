6b5613fdc894af1ba24a0937bc17c0e2de5fd33b
JuanGT01 <jgato@iesflorenciopintado.es>
2021-09-22 11:22:14 +0200

    Mandando valores al activity

--- app/src/main/java/com/example/jugandoconactividades/MainActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/MainActivity.java
@@ -6,18 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button lanzar;
+    EditText nombre;
+    public static final String Nombre="NOMBRE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lanzar= findViewById(R.id.buttonLanzar);
+        nombre = findViewById(R.id.Nombre);
         lanzar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this, SaludoActivity.class);
+                intent.putExtra(Nombre,nombre.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SaludoActivity extends AppCompatActivity {
     Button volver;
+    TextView saludo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+        Intent intent = getIntent();
+        saludo= findViewById(R.id.Saludo);
+        String nombre = intent.getStringExtra(MainActivity.Nombre);
+        saludo.setText("Hola "+nombre);
         volver= findViewById(R.id.buttonVolver);
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
