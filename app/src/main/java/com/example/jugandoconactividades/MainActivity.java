fa5574ec0e4ded718c715668b2d9aac0e67f11bd
JuanGT01 <jgato@iesflorenciopintado.es>
2021-09-23 09:11:52 +0200

    Capturo valores de otra activity

--- app/src/main/java/com/example/jugandoconactividades/MainActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jugandoconactividades;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button lanzar;
     EditText nombre;
     public static final String Nombre="NOMBRE";
+    public static final String Numero="numero";
+    public static final int CODIGO_VUELTA_PARA_SALUDO=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this, SaludoActivity.class);
                 intent.putExtra(Nombre,nombre.getText().toString());
-                startActivity(intent);
+                startActivityForResult(intent,CODIGO_VUELTA_PARA_SALUDO);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODIGO_VUELTA_PARA_SALUDO && resultCode== RESULT_OK){
+            nombre.setText(nombre.getText()+" indico el numero "+data.getStringExtra(Numero));
+        }
     }
 }
--- app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SaludoActivity extends AppCompatActivity {
     Button volver;
     TextView saludo;
+    EditText num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +22,14 @@ public class SaludoActivity extends AppCompatActivity {
         String nombre = intent.getStringExtra(MainActivity.Nombre);
         saludo.setText("Hola "+nombre);
         volver= findViewById(R.id.buttonVolver);
+        num=findViewById(R.id.editTextNumero);
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                num= findViewById(R.id.editTextNumero);
+                Intent intento= new Intent();
+                intento.putExtra(MainActivity.Numero,num.getText().toString());
+                setResult(RESULT_OK,intento);
                 finish();
             }
         });
